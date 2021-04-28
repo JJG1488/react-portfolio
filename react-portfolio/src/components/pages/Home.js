43a8d7f6a2c203c301b7e37eaf2666c9a0d3623c
James <jamesgault1488@gmail.com>
2021-04-28 14:10:53 -0400

    Home.js page set up

---
+++ react-portfolio/src/components/pages/Home.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+    <div className="container">
+
+      <div className="row">
+
+        <div className="contact-col">
+          <img src="./Assets/Images/read.PNG" alt="dev-reading" id="reading-photo" />
+
+          <p>
+            Thank you! Why am I saying thank you? You have taken time out of your busy schedule to have a look at my portfolio's home page. You could be reading anything in the world, yet, you've chosen to spend time looking through some of my material and I'm grateful. Please, if inspired by curiosity from any aspect of viewing my content, you can see more of my wolk via the github link on my contacts page. If you are looking for a direct (fast) response, the best place to reach me is linkedin. That link is also on my contact page. Good day to you and I hope that you find something that you like.
+    </p>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+);
+
+export default Home;
