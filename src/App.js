e4c6636136f02022ba4f76aecdab85b9b59354c2
James <jamesgault1488@gmail.com>
2021-04-29 13:49:41 -0400

    restructured files and folders to have readme in the correct position for github

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React from "react";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import NavTabs from "./components/NavTabs";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Portfolio from "./components/pages/Portfolio";
 import Contact from "./components/pages/Contact";
-// import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
