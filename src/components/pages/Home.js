3876d90ca0fcb539bacbce64a7b98049a041689b
James <jamesgault1488@gmail.com>
2021-04-29 16:51:39 -0400

    fixed spelling error on home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -11,7 +11,7 @@ const Home = () => (
           <img src="./Assets/Images/read.PNG" alt="dev-reading" id="reading-photo" />
 
           <p>
-            Thank you! Why am I saying thank you? You have taken time out of your busy schedule to have a look at my portfolio's home page. You could be reading anything in the world, yet, you've chosen to spend time looking through some of my material and I'm grateful. Please, if inspired by curiosity from any aspect of viewing my content, you can see more of my wolk via the github link on my contacts page. If you are looking for a direct (fast) response, the best place to reach me is linkedin. That link is also on my contact page. Good day to you and I hope that you find something that you like.
+            Thank you! Why am I saying thank you? You have taken time out of your busy schedule to have a look at my portfolio's home page. You could be reading anything in the world, yet, you've chosen to spend time looking through some of my material and I'm grateful. Please, if inspired by curiosity from any aspect of viewing my content, you can see more of my work via the github link on my contacts page. If you are looking for a direct (fast) response, the best place to reach me is linkedin. That link is also on my contact page. Good day to you and I hope that you find something that you like.
     </p>
 
         </div>
