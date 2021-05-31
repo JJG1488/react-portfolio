e274347b2b0984664fe4a99492e92aae34cb7223
James <jamesgault1488@gmail.com>
2021-05-31 10:18:06 -0400

    adding assets

---
+++ src/components/pages/About.js
@@ -0,0 +1,80 @@
+import React from "react";
+
+const About = () => (
+  <div>
+    <h1>About Page</h1>
+    {/* <p>
+      Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui mauris,
+      ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus porta. Nam
+      quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
+      imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed rhoncus mollis
+      diam, sit amet facilisis lectus blandit at.
+    </p> */}
+
+
+    <div className="container">
+
+
+        <div className="row">
+
+            <div className="col-xs-12">
+
+    
+
+                <img src="./Assets/Images/electric-man.PNG" alt="electrician" id="dev-photo" />
+
+
+                <p>
+
+                    I wear many hats in my everyday life. Whether its engineering a company's network, doing electrical
+                    work for security systems, working wood
+                    in my garage, learning and applying web development skills, or just being the
+                    loving and caring father that I am everyday, I love what I do and I do it all with passion and
+                    purpose. I had an ios application out in 2018, but
+                    I didn't feel good about the project because I did not build the project myself and was not entirely
+                    in control of the creative process. I love being creative and being able to express my creativity
+                    even more. I am infatuated with the "click" moments that you get when things begin to
+                    sink in and make sense. So naturally, I picked up every book that I could about coding and began to
+                    read them.
+                    There was this idea that I could build my own applications and submit them to the world to see that
+                    became
+                    a source for my drive to learn more about coding. Then I realized that this is a skill that can be
+                    passed along, and
+                    from that, purpose was added to my drive to learn.
+
+                </p>
+
+                <p>
+
+                    Development in general is more than a hobby or skill to me. It is a tool that can be used to change
+                    the lives of any and everyone. I equate developing to having a super power. To be able to work with
+                    people or companies to help them make their dreams or products come to life is a big deal to me.
+                    Just as
+                    someone took the time out to help me bring my application to life, which was my dream at that
+                    moment, I
+                    want to do the same and help people to build and create tools and applications that can fulfill
+                    their
+                    dreams and help make the world a better place. I'm excited to hear about new ideas and helping to
+                    come
+                    up with ways to bring them to life.
+
+                </p>
+
+                <hr/>
+
+            </div>
+
+            <hr/>
+
+            
+
+            <hr/>
+
+        </div>
+
+    
+    </div>
+  </div>
+);
+
+export default About;
