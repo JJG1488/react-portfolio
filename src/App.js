583812fc4a36bc9d95a012d680b46bc55237a5ff
James <74686199+JJG1488@users.noreply.github.com>
2021-06-15 16:35:21 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import NavTabs from "./components/NavTabs";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
@@ -17,11 +17,13 @@ function App() {
     <Router>
         <NavTabs />
         <Header />
+        <Switch>
         <Route exact path="/" component={Home} /> 
         <Route exact path="/home" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/portfolio" component={Portfolio} />
         <Route exact path="/contact" component={Contact} />
+        </Switch>
         <Footer />
 </Router>
     </Wrapper>
