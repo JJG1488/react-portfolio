4aedf6c9047f71564ee799999708b685852f4a8c
James Gault <peregrinefalcon@peregrinefalcon.local>
2022-03-01 09:04:22 -0500

    placed margin on top of certificate image and contact information

--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -1,38 +1,49 @@
 import React from "react";
-import './styles.css';
+import "./styles.css";
 
 const Contact = () => (
   <div>
-
-    <div className="container">
+    <div className="contact-container">
       {/* <div className="row">
         <div className="col"> */}
       <span className="contact-info">
-
         Name: James J. Gault
         <br />
-
         Email:
-        <a href="mailto:jamesgault1488@gmail.com=My custom mail body">  jamesgault1488@gmail.com</a>
-
+        <a href="mailto:jamesgault1488@gmail.com=My custom mail body">
+          {" "}
+          jamesgault1488@gmail.com
+        </a>
         <br />
-        <a href="https://www.linkedin.com/in/jamesgault1488">  <span id="social-link-data">LinkedIn: </span>  <i className="fa fa-linkedin"
-          aria-hidden="true"></i></a>
-
+        <a href="https://www.linkedin.com/in/jamesgault1488">
+          {" "}
+          <span id="social-link-data">LinkedIn: </span>{" "}
+          <i className="fa fa-linkedin" aria-hidden="true"></i>
+        </a>
         <br />
-        <a href="https://github.com/JJG1488">  <span id="social-link-data">GitHub: </span>  <i className="fa fa-github" aria-hidden="true"></i></a>
-
+        <a href="https://github.com/JJG1488">
+          {" "}
+          <span id="social-link-data">GitHub: </span>{" "}
+          <i className="fa fa-github" aria-hidden="true"></i>
+        </a>
         <br />
-        <a href="https://twitter.com/JamesJGault"> <span id="social-link-data">  Twitter: </span> <i className="fa fa-twitter-square" aria-hidden="true"></i></a>
-
+        <a href="https://twitter.com/JamesJGault">
+          {" "}
+          <span id="social-link-data"> Twitter: </span>{" "}
+          <i className="fa fa-twitter-square" aria-hidden="true"></i>
+        </a>
         <br />
-
-        <a href="https://1drv.ms/w/s!AoCauJZ3EHFdgwMB4s6gnDG-WQOe" rel="noreferrer" target="_blank"> <span id="social-link-data">Open Resume in a PDF File </span>  <i className="fa fa-download" aria-hidden="true"></i></a>
-
+        <a
+          href="https://1drv.ms/w/s!AoCauJZ3EHFdgwMB4s6gnDG-WQOe"
+          rel="noreferrer"
+          target="_blank"
+        >
+          {" "}
+          <span id="social-link-data">Open Resume in a PDF File </span>{" "}
+          <i className="fa fa-download" aria-hidden="true"></i>
+        </a>
         <br />
-
         Phone: 586-229-6418
-
       </span>
     </div>
   </div>
