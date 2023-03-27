92563e5b033bf77e69b1ac3ef5ce67fd609fe198
James Gault <peregrinefalcon@peregrinefalcon.local>
2023-03-27 16:35:09 -0400

    added switch in place of Routes in react-router-dom in App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Footer from "./components/Footer/Footer";
 import Wrapper from "./components/Wrapper";
 // import Header from "./components/Header/Header";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Switch, Routes } from "react-router-dom";
 import Home from "./components/pages/Home/Home";
 import About from "./components/pages/About/About";
 import Portfolio from "./components/pages/Portfolio/Portfolio";
@@ -16,13 +16,13 @@ function App() {
         {/* <Header /> */}
         <NavTabs />
 
-        <Routes>
+        <Switch>
           <Route path="/" element={<Portfolio />} />
           <Route path="home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/contact" element={<Contact />} />
-        </Routes>
+        </Switch>
         <Footer />
       </Wrapper>
     </Router>
